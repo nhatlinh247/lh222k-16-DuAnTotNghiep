6022ac671c8f86e94fb0680b5d4ceaa5bb4a0ae1
lh222k-16 <57992283+lh222k-16@users.noreply.github.com>
2020-11-26 10:36:28 +0700

    huy

---
+++ pages/js/app.js
@@ -0,0 +1,53 @@
+// Single Page Application
+var app = angular.module('myApp', ['ngRoute'])
+app.config(function($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: '../pages/home.html'
+        })
+        .when('/gioithieu', {
+            templateUrl: '../pages/gioithieu.html'
+        })
+        
+        .when('/product', {
+            templateUrl: '../pages/product.html'
+        });
+    // .otherwise({
+    //     redirectTo: '/'
+    // });
+});
+//Filter String
+app.filter('cut', function() {
+    return function(value, wordwise, max, tail) {
+        if (!value) return '';
+
+        max = parseInt(max, 10);
+        if (!max) return value;
+        if (value.length <= max) return value;
+
+        value = value.substr(0, max);
+        if (wordwise) {
+            var lastspace = value.lastIndexOf(' ');
+            if (lastspace !== -1) {
+                if (value.charAt(lastspace - 1) === '.' || value.charAt(lastspace - 1) === ',') {
+                    lastspace = lastspace - 1;
+                }
+                value = value.substr(0, lastspace);
+            }
+        }
+
+        return value + (tail || ' …');
+    };
+});
+//SweetAlert
+app.controller('memberLogin', function($scope) {
+    $scope.submit = function() {
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+        })
+    }
+})
